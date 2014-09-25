9801b2bbbcddcc3182fdb8127d9f472d86e6e445
Petr Krulis <hello@petrkrulis.cz>
2014-09-25 21:18:58 +0200

    Fixes, removed dependencies, etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,12 +21,7 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc'
       }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+    }
 
     // Configuration to be run (and then tested).
     node_auto_deploy: {
@@ -42,11 +37,6 @@ module.exports = function(grunt) {
           ssh: 'root@nodejs.droplet'
         }
       },
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
 
   });
@@ -54,14 +44,9 @@ module.exports = function(grunt) {
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'node_auto_deploy']);
+  grunt.registerTask('test', ['node_auto_deploy']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
